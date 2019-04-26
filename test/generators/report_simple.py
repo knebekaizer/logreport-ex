187d8ae685aa4fab81445c3af8f48c0cf5044164
vladimir.ivanov <v.ivanov@socialquantum.com>
2019-04-26 23:17:42 +0300

    minor: params in py report

--- test/generators/report_simple.py
+++ test/generators/report_simple.py
@@ -8,7 +8,7 @@ from ipaddress import IPv4Address, IPv6Address, ip_network
 
 registry = []
 
-for line in open("../c5.txt"):
+for line in open("c3.txt"):
     name, addr = line.split()
     registry.append( (name, ip_network(addr)) )
 
@@ -20,7 +20,7 @@ report = {}
 #print(report)
 
 lines = 0
-for line in open("../iplog.dat"):
+for line in sys.stdin:
     addr, bytes = line.split()
     bytes = int(bytes)
     if not bytes: continue
