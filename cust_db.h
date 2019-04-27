bb92cb92f17e5c7e3bb181e85485d9dfb043f4f2
vladimir.ivanov <knebekaizer@gmail.com>
2019-04-27 12:05:30 +0300

    cleanup

--- cust_db.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Vladimir Ivanov on 2019-04-22.
-//
-
-#ifndef IPLOG_TEST_CUST_DB_H
-#define IPLOG_TEST_CUST_DB_H
-
-#endif //IPLOG_TEST_CUST_DB_H
--- report.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Vladimir Ivanov on 2019-04-22.
-//
-
-#ifndef IPLOG_TEST_REPORT_H
-#define IPLOG_TEST_REPORT_H
-
-#endif //IPLOG_TEST_REPORT_H
